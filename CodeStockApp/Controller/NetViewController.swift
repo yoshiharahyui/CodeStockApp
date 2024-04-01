619ae7cde718a055036a37dff9e8911aaa6caf33
yoshiharahyui <1015hyui@gmail.com>
2024-04-01 12:35:51 +0900

    NetViewController用のparchment実装完了

--- CodeStockApp/Controller/NetViewController.swift
+++ CodeStockApp/Controller/NetViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Parchment
 
 class NetViewController: UIViewController {
     override func viewDidLoad() {
@@ -21,5 +22,47 @@ class NetViewController: UIViewController {
         appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.standardAppearance = appearance
         self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        configureparchment()
+    }
+    
+    //Parchmentを使ったタブ作成
+    func configureparchment() {
+        let springstoryboard = UIStoryboard(name: "SpringView", bundle: nil)
+        let springvc = springstoryboard.instantiateViewController(withIdentifier: "Spring") as! SpringViewController
+        
+        let summerstoryboard = UIStoryboard(name: "SummerView", bundle: nil)
+        let summervc = summerstoryboard.instantiateViewController(withIdentifier: "Summer") as! SummerViewController
+        
+        let fallstoryboard = UIStoryboard(name: "FallView", bundle: nil)
+        let fallvc = fallstoryboard.instantiateViewController(withIdentifier: "Fall") as! FallViewController
+        
+        let winterstoryboard = UIStoryboard(name: "WinterView", bundle: nil)
+        let wintervc = winterstoryboard.instantiateViewController(withIdentifier: "Winter") as! WinterViewController
+        
+        springvc.title = "Spring"
+        summervc.title = "Summer"
+        fallvc.title = "Fall"
+        wintervc.title = "Winter"
+        
+        //インスタンスを作成して4つのVCを配列に入れ込む
+        let pagingViewController = PagingViewController(viewControllers: [ springvc, summervc, fallvc, wintervc ])
+        //メニューアイテムの色設定
+        pagingViewController.menuItemSpacing = -40
+        pagingViewController.indicatorColor = .white
+        pagingViewController.borderColor = .white
+        pagingViewController.menuBackgroundColor = .black
+        pagingViewController.textColor = .white
+        pagingViewController.selectedTextColor = .darkGray
+        //インスタンスをChildViewControllerに入れる
+        addChild(pagingViewController)
+                view.addSubview(pagingViewController.view)
+                pagingViewController.didMove(toParent: self)
+                pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+                pagingViewController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+                pagingViewController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+                pagingViewController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+                pagingViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
     }
 }
