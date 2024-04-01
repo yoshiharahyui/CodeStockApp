834547f84e917d010ae09e1c0c06aaab6f427c29
yoshiharahyui <1015hyui@gmail.com>
2024-04-01 12:59:57 +0900

    indicatorの横の長さ調整

--- CodeStockApp/Controller/NetViewController.swift
+++ CodeStockApp/Controller/NetViewController.swift
@@ -50,6 +50,7 @@ class NetViewController: UIViewController {
         //メニューアイテムの色設定
         pagingViewController.menuItemSpacing = -40
         pagingViewController.indicatorColor = .white
+        pagingViewController.indicatorOptions = .visible(height: 4, zIndex: Int.max, spacing: UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15), insets: UIEdgeInsets())
         pagingViewController.borderColor = .white
         pagingViewController.menuBackgroundColor = .black
         pagingViewController.textColor = .white
