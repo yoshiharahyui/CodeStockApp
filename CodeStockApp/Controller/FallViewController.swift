5989aa5c116d33b4b56bfc15375553011099386b
yoshiharahyui <1015hyui@gmail.com>
2024-04-02 14:44:03 +0900

    FallViewのカスタムセル実装

--- CodeStockApp/Controller/FallViewController.swift
+++ CodeStockApp/Controller/FallViewController.swift
@@ -8,6 +8,30 @@
 import Foundation
 import UIKit
 
-class FallViewController: UIViewController {
+class FallViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //セルの登録
+        tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
+        cell.datelabel.text = "2024/04/03"
+        cell.imageview.image = UIImage(systemName: "swift")
+        cell.memolabel.text = "ううううううううううううう"
+        //セルの背景色変更
+        cell.backgroundColor = UIColor(red: 203/255, green: 155/255, blue: 97/255, alpha: 1.0)
+        //セルを選択不可
+        cell.isUserInteractionEnabled = false
+        return cell
+    }
     
 }
