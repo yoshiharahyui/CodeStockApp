cc26191def8514815a663459b0b279f14fa8f0dd
yoshiharahyui <1015hyui@gmail.com>
2024-04-07 14:56:01 +0900

    UIImageViewを追加した

--- CodeStockApp/Controller/AddViewController.swift
+++ CodeStockApp/Controller/AddViewController.swift
@@ -9,6 +9,10 @@ import Foundation
 import UIKit
 
 class AddViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func postButton(_ sender: Any) {
+    }
     @IBAction func cancelButton(_ sender: Any) {
         self.dismiss(animated: true,completion: nil)
     }
