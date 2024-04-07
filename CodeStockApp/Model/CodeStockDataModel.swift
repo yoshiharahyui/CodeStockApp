a270c5f542b921a253bdfd430589842c8e6c140a
yoshiharahyui <1015hyui@gmail.com>
2024-04-07 16:17:41 +0900

    CodeStockDataModelにデータモデルを追加

--- CodeStockApp/Model/CodeStockDataModel.swift
+++ CodeStockApp/Model/CodeStockDataModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import RealmSwift
+
+class CodeStockDataModel: Object {
+    //データを一意に識別するための識別子
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var imageData: Data?
+    @objc dynamic var memotext: String = ""
+    @objc dynamic var recordDate: Date = Date()
+}
