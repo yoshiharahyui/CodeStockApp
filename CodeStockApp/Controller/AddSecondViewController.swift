5bb5b847729c1461787021aef41594577752d012
yoshiharahyui <1015hyui@gmail.com>
2024-04-11 14:31:34 +0900

    AddSecondViewのstoryboard作成,UI部品接続

--- CodeStockApp/Controller/AddSecondViewController.swift
+++ CodeStockApp/Controller/AddSecondViewController.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+import RealmSwift
+
+class AddSecondViewController: UIViewController {
+    @IBOutlet weak var memoTextView: UITextView!
+    
+    @IBAction func addImageButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func postAction(_ sender: Any) {
+    }
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var selectSeasonButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
