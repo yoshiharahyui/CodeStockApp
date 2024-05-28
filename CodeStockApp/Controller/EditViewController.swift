d0b9b2fab495627554a927c44b0510ddcb874fb5
yoshiharahyui <1015hyui@gmail.com>
2024-05-28 14:25:05 +0900

    EditfileのIB接続完了

--- CodeStockApp/Controller/EditViewController.swift
+++ CodeStockApp/Controller/EditViewController.swift
@@ -10,5 +10,15 @@ import UIKit
 import RealmSwift
 
 class EditViewController: UIViewController, UITextDragDelegate {
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true,completion: nil)
+    }
+    @IBAction func postButton(_ sender: Any) {
+    }
+    @IBOutlet weak var imageView: UIImageView!
     
+    @IBAction func addImageButtonAction(_ sender: Any) {
+    }
+    @IBOutlet weak var selectSeasonButton: UIButton!
+    @IBOutlet weak var memoTextView: HintTextView!
 }
