092de32c3bd4174e5a72a509a46f53320902314b
yoshiharahyui <1015hyui@gmail.com>
2024-06-07 09:33:54 +0900

    CodeStockSecondDataModel追加

--- CodeStockApp/Model/CodeStockSecondDataModel.swift
+++ CodeStockApp/Model/CodeStockSecondDataModel.swift
@@ -8,8 +8,29 @@
 import Foundation
 import RealmSwift
 
-class CodeStockSecondDataModel: Object {
+class SpringCodeStockSecondDataModel: Object {
     //データを一意に識別するための識別子
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var imageData: Data?
+    @objc dynamic var memotext: String = ""
+    @objc dynamic var recordDate: Date = Date()
+}
+
+class SummerCodeStockSecondDataModel: Object {
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var imageData: Data?
+    @objc dynamic var memotext: String = ""
+    @objc dynamic var recordDate: Date = Date()
+}
+
+class FallCodeStockSecondDataModel: Object {
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var imageData: Data?
+    @objc dynamic var memotext: String = ""
+    @objc dynamic var recordDate: Date = Date()
+}
+
+class WinterCodeStockSecondDataModel: Object {
     @objc dynamic var id: String = UUID().uuidString
     @objc dynamic var imageData: Data?
     @objc dynamic var memotext: String = ""
