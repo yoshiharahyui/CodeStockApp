8e0472217042a49877a295b42596800b613ec8cd
yoshiharahyui <1015hyui@gmail.com>
2024-06-12 14:56:10 +0900

    UIView.animateでスプラッシュ画面作成

--- CodeStockApp/Controller/SplashViewController.swift
+++ CodeStockApp/Controller/SplashViewController.swift
@@ -13,19 +13,32 @@ class SplashViewController: UIViewController {
    // @IBOutlet var splashView: LottieAnimationView!
         
     @IBOutlet var splashView: UIView!
+    @IBOutlet weak var titleLogo: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.titleLogo.alpha = 0
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+            UIView.animate(withDuration: 2.0, delay: 0.5, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.0, options: []) {
+                self.titleLogo.alpha = 1
+                self.titleLogo.frame.origin.y += 350
+        } completion: { (_) in
+            UIView.animate(withDuration: 1) {
+                self.titleLogo.alpha = 0.0
+            } completion: { _ in
+                self.titleLogo.alpha = 0.0
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-                //splashView.contentMode = .scaleAspectFit
-                //splashView.loopMode = .loop
-                //splashView.play()
         //2秒後にsplashViewを削除
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.2) {
             self.splashView.removeFromSuperview()
             
             let vc = MainTabBarController()
-//            vc.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
-//            vc.modalPresentationStyle = .fullScreen
-//            self.present(vc, animated: true)
             let sceneDelegate = UIApplication.shared.connectedScenes
                     .first!.delegate as! SceneDelegate
             sceneDelegate.window!.rootViewController = vc
